3ac93d404f870b4b1db8429d4a37f56ebcc736fb
ooaventur <ooaventur@gmail.com>
2025-09-15 23:01:47 +0200

    Update __init__.py

--- autopost/__init__.py
+++ autopost/__init__.py
@@ -1,3 +1,6 @@
 """Utility package for AventurOO autopost scripts."""
 
-__all__ = []
+# Single shared "seen" database file used by all autopost scripts.
+SEEN_DB_FILENAME = "seen_all.json"
+
+__all__ = ["SEEN_DB_FILENAME"]
