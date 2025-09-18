08b07d33bfa9e1e75a24affbc7a7d731078e2a49
ooaventur <ooaventur@gmail.com>
2025-09-18 20:53:21 +0200

    Create site.js

---
+++ src/site/_data/site.js
@@ -0,0 +1,6 @@
+module.exports = {
+  name: "AventurOO",
+  description: "AventurOO curates top stories across news, technology & AI, travel inspiration, culture, lifestyle tips, and shopping deals in one streamlined hub.",
+  url: "https://aventuroo.com",
+  author: "AventurOO"
+};
